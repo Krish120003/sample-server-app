4e387938dcdf27accf327046d7159e8adfd2fa53
Krish120003 <krish120003@gmail.com>
2025-03-04 14:39:52 -0500

    a

--- src/pages/ssr/index.tsx
+++ src/pages/ssr/index.tsx
@@ -21,6 +21,7 @@ export default function SSRTime({ currentTime }: ServerTimeProps) {
     <div className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold">Server-Side Rendered Time</h1>
       <p>Current server time: {currentTime}</p>
+      <p>build new wow</p>
     </div>
   );
 }
