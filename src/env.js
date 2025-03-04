4724af463aae3a42c36d0d85f0c95da6057462ef
Krish120003 <krish120003@gmail.com>
2025-03-04 01:05:58 -0500

    feat: say hi to env:DISPLAY_NAME :)

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    DISPLAY_NAME: z.string(),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    DISPLAY_NAME: process.env.DISPLAY_NAME,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
