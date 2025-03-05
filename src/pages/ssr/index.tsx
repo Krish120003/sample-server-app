1b6fd9ace3d3fcb0b8bc8bc05a2249a63217692d
Krish120003 <krish120003@gmail.com>
2025-03-04 19:13:04 -0500

    remove sample text

--- src/pages/ssr/index.tsx
+++ src/pages/ssr/index.tsx
@@ -21,7 +21,6 @@ export default function SSRTime({ currentTime }: ServerTimeProps) {
     <div className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold">Server-Side Rendered Time</h1>
       <p>Current server time: {currentTime}</p>
-      <p>build new wow</p>
     </div>
   );
 }
